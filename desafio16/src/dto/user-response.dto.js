2c46314c58f9211461d4a6c50864a9130d615481
Mercedes Pilar Miquelez <112575469+mercedesmiquelez@users.noreply.github.com>
2024-09-19 10:41:55 -0300

    Delete desafio16 directory

--- desafio16/src/dto/user-response.dto.js
+++
@@ -1,7 +0,0 @@
-export const userResponseDto = (user) => {
-  return {
-    first_name: user.first_name,
-    email: user.email,
-    role: user.role,
-  };
-};
--- desafio16/src/persistences/mongo/repositories/ticket.repository.js
+++
@@ -1,9 +0,0 @@
-import { ticketModel } from "../models/ticket.model.js";
-
-const create = async (data) => {
-  return await ticketModel.create(data);
-};
-
-export default {
-  create,
-};
